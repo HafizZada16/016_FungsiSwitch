fd9903e25dbacb39b05d1a8519bb0d3a3bb1df0f
HafizZada16 <hafizzada16@gmail.com>
2024-03-26 17:35:47 +0700

    menambah 4 proses

--- FungsiSwitch/FungsiSwitch.cpp
+++ FungsiSwitch/FungsiSwitch.cpp
@@ -3,7 +3,33 @@ using namespace std;
 
 int bilA, bilB;
 
-void input
+void input()
+{
+    cout << "Masukkan bilangan Pertama = ";
+    cin >> bilA;
+    cout << "Masukkan bilangan Kedua = ";
+    cin >> bilB;
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+float penjumlahan(float a, float b)
+{
+    return a / b;
+}
 
 int main()
 {
